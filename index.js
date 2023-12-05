eb44c04d4f6432f0b06cd185ccf668e2b68f12df
edelciomolina <edelciomolina@gmail.com>
2023-12-05 19:09:50 -0300

    Add: You are here

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const delete_input_files = (core.getInput('delete_input_files') || 'true') === '
 const create_bundle_config = (core.getInput('create_bundle_config') || 'true') === 'true'
 
 try {
+    core.info(`✅ You are here "${process.cwd()}"`)
+
     core.info(`✅ Input Parameters`)
     core.info(` - bundle_config_folder: ${bundle_config_folder}`)
     core.info(` - create_bundle_config: ${create_bundle_config}`)
